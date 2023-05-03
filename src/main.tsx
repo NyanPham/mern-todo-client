02f4c5793d053d2d4e21c298f61bb950e962d0bb
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2023-05-03 16:59:00 +0700

    clearning for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,13 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
-import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <CookiesProvider>
-                <Router>
-                    <App />
-                </Router>
-            </CookiesProvider>
+            <Router>
+                <App />
+            </Router>
         </Provider>
     </React.StrictMode>
 )
