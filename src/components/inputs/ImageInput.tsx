4eb1355b5136afce2d674a547806b89595fb955b
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2023-05-06 18:36:14 +0700

    more responsive now, need to work on image fetching, deploy

--- src/components/inputs/ImageInput.tsx
+++ src/components/inputs/ImageInput.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import FileInput, { FileInputProps } from './FileInput'
-import { IFile } from '../../types'
+// import { IFile } from '../../types'
 import Avatar from '../Avatar'
 
 type ImageInputProps = FileInputProps & { onImageChosen: (file: File) => void }
 
 const ImageInput: React.FC<ImageInputProps> = ({ onImageChosen, ...props }) => {
-    const [currentImage, setCurrentImage] = useState<File>()
+    // const [currentImage, setCurrentImage] = useState<File>()
     const [previewImage, setPreviewImage] = useState<string>('')
-    const [progress, setProgress] = useState<number>(0)
-    const [message, setMessage] = useState<string>('')
+    // const [progress, setProgress] = useState<number>(0)
+    // const [message, setMessage] = useState<string>('')
 
-    const [imageInfos, setImageInfos] = useState<Array<IFile>>([])
+    // const [imageInfos, setImageInfos] = useState<Array<IFile>>([])
 
     const selectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFiles = e.target.files as FileList
-        setCurrentImage(selectedFiles?.[0])
+        // setCurrentImage(selectedFiles?.[0])
         setPreviewImage(URL.createObjectURL(selectedFiles?.[0]))
-        setProgress(0)
+        // setProgress(0)
 
         if (selectedFiles[0]) {
             onImageChosen(selectedFiles[0])
